d1b518a424d69e81cb5c4ae4bbd4a159a136f695
Shanghaai <34318533+Shanghaai@users.noreply.github.com>
2017-12-13 12:21:54 +0530

    Add files via upload

---
+++ CreateObjectAtRunTime/src/sahil/tundla/Object/ObjectAt.java
@@ -0,0 +1,25 @@
+package sahil.tundla.Object;
+
+
+class ObjectAt 
+{
+	public static void main(String[] args) throws Exception 
+	{
+		Object o = Class.forName(args[0]).newInstance();
+		System.out.println("Object Created for : "+o.getClass().getName());
+		
+		
+		String s = "sahil";
+		
+		
+		
+		
+	}
+
+class sahil
+{}
+class wasim
+{}
+class emp
+{}
+}
